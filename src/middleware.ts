8e98490e038ad0122396524677f716513620e30b
Kumar Raghav Choudhary <raghavchy1511@gmail.com>
2024-03-23 12:36:48 +0530

    solved header image issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,6 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|.*\\.png$|login_img\\.jpg|header_bg\\.svg).*)",
+    "/((?!api|_next/static|_next/image|.*\\.png$|login_img\\.jpg|header-bg\\.svg).*)",
   ],
 };
